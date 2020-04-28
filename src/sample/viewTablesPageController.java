2c1bb4900bcdc7123349ac10dd511fa526eb3bd8
amarmharris <kb24amar@gmail.com>
2020-04-28 07:43:49 -0500

    2ND commit

--- src/sample/viewTablesPageController.java
+++ src/sample/viewTablesPageController.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +15,19 @@ public class viewTablesPageController implements Initializable {
     public Tab itemsPage;
     public Tab mediaPage;
     public Tab booksPage;
+    public TableView mediaTable;
+    public TableColumn mediaIDCol;
+    public TableColumn mediaTtleCol;
+    public TableColumn directorCol;
+    public TableColumn mediaDoeCol;
+    public TableColumn mediaStatusCol;
+    public TableView bookTable;
+    public TableColumn bookIDCol;
+    public TableColumn bookTitleCol;
+    public TableColumn authorCol;
+    public TableColumn bookDoeCol;
+    public TableColumn bookStatusCol;
+    public TabPane tabPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,4 +49,6 @@ public class viewTablesPageController implements Initializable {
     public void booksPage(Event event) {
     }
 
+    public void back2Home(ActionEvent actionEvent) {
+    }
 }
